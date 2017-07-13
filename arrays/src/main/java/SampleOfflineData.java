4cb0f8c7d8f421765fe0c617d8fa8c3f1d878b6c
Joey Bennett <jebennett099@gmail.com>
2017-07-13 16:36:15 -0700

    Completed 5.12 and 5.18

--- arrays/src/main/java/SampleOfflineData.java
+++ arrays/src/main/java/SampleOfflineData.java
@@ -1,12 +1,26 @@
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 public class SampleOfflineData {
 
     /*
-    5.11
+    5.12
     */
 
     public static void randomSampling(int k, List<Integer> list) {
+        final Random rnd = new Random();
+        final Set<Integer> set = new HashSet<>();
+        int n = list.size();
 
+        for (int i = n - k; i < k; i++) {
+            int r = list.get(rnd.nextInt(i));
+            if (!set.contains(r)) {
+                set.add(r);
+            } else {
+                set.add(list.get(i));
+            }
+        }
     }
 }
--- arrays/src/main/java/SudokuChecker.java
+++ arrays/src/main/java/SudokuChecker.java
@@ -7,6 +7,55 @@ public class SudokuChecker {
     */
 
     public static boolean isValidSudoku(List<List<Integer>> partialAssignment) {
+        // Invalid input
+        if (partialAssignment == null || partialAssignment.size() == 0 ||
+                partialAssignment.size() != 9 || partialAssignment.get(0).size() != 9) {
+            return false;
+        }
+
+        int[] rows = new int[10]; // 9 + 1
+        int[] cols = new int[10];
+
+        // First validate against the rows and cols
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (partialAssignment.get(i).get(j) == 0) continue;
+
+                int num = partialAssignment.get(i).get(j);
+                if (rows[num] != 0) return false;
+                rows[num] = num;
+
+                num = partialAssignment.get(j).get(i);
+                if (cols[num] != 0) return false;
+                cols[num] = num;
+            }
+            rows = new int[10];
+            cols = new int[10];
+        }
+
+        // Then check every square
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                if (!isValidSudokuSquare(partialAssignment, i, j))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isValidSudokuSquare(List<List<Integer>> partialAssignment, int row, int col) {
+        int[] square = new int[10];
+
+        for (int i = row; i < row + 3; i++) {
+            for (int j = col; j < col + 3; j++) {
+                int num = partialAssignment.get(i).get(j);
+                if (square[num] != 0) {
+                    return false;
+                }
+                square[num] = num;
+            }
+        }
 
         return true;
     }
